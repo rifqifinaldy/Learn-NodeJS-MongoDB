6dd54aac602879d0a588bb410069750d05727eb9
rifqifinaldy <rifqifinaldy@gmail.com>
2022-03-16 14:40:22 +0700

    add Base MVC to project

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
-const { render } = require("express/lib/response");
+const blogsRoutes = require("./routes/blogRoutes");
 
 // Express App
 const app = express();
@@ -21,7 +19,6 @@ app.set("view engine", "ejs");
 // middleware & static files
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
 
 // routes
 app.get("/", (req, res) => {
@@ -33,58 +30,7 @@ app.get("/about", (req, res) => {
 });
 
 // blog routes
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All Blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.post("/blogs", (req, res) => {
-  const blog = new Blog(req.body);
-
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.delete("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({
-        redirect: "/blogs",
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a New Blog" });
-});
+app.use("/blogs", blogsRoutes);
 
 // 404
 app.use((req, res) => {
