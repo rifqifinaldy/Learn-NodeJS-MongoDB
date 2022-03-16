61dee98f50ebbe9f41f9318acd5af8c3a34a25d9
rifqifinaldy <rifqifinaldy@gmail.com>
2022-03-16 10:25:16 +0700

    add middleware for static files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 // Express App
 const app = express();
@@ -12,14 +13,9 @@ const dirname = { root: __dirname };
 // Listen for Request
 app.listen(3000);
 
-app.use((req, res, next) => {
-  console.log("new request made :");
-  console.log("host :", req.hostname);
-  console.log("path :", req.path);
-  console.log("method :", req.method);
-  next();
-});
-
+// middleware & static files
+app.use(express.static("public"));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const blogs = [
