d041b27639f8ccbdf7fd41e8d31d1b7b9e2ae97e
rifqifinaldy <rifqifinaldy@gmail.com>
2022-03-16 13:33:55 +0700

    ADD create update delete for blog

--- app.js
+++ app.js
@@ -1,42 +1,85 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+const { render } = require("express/lib/response");
 
 // Express App
 const app = express();
 
+// connect to mongoDB
+const dbURI =
+  "mongodb+srv://rifqifinaldy:25maret1997@cluster0.socjk.mongodb.net/rifqif_blogs?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 // register view engine
 app.set("view engine", "ejs");
 
-// Directory Name
-const dirname = { root: __dirname };
-
-// Listen for Request
-app.listen(3000);
-
 // middleware & static files
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+// routes
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Rifqi Belajar EJS",
-      snippet: "LOREM IPSUM DOLOR",
-    },
-    {
-      title: "Rifqi Belajar MongoDB",
-      snippet: "DOLOR SIT AMET",
-    },
-    {
-      title: "Rifqi",
-      snippet: "AH",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+// blog routes
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({
+        redirect: "/blogs",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
---
+++ models/blog.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Define the structure of the document
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    snippet: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Blog = mongoose.model('Blog', blogSchema);
+module.exports = Blog;
