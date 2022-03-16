31f44f866b03dd74ff6679550e0e61d8bac4542f
rifqifinaldy <rifqifinaldy@gmail.com>
2022-03-16 18:27:26 +0700

    SASS BASE CONFIGURATION 1.0

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const blogsRoutes = require("./routes/blogRoutes");
+const sassMiddleware = require('node-sass-middleware')
 
 // Express App
 const app = express();
@@ -16,6 +17,13 @@ mongoose
 // register view engine
 app.set("view engine", "ejs");
 
+app.use(
+  sassMiddleware({
+    src: __dirname + '/sass',
+    dest: __dirname + '/public',
+  })
+);
+
 // middleware & static files
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -23,6 +31,10 @@ app.use(express.urlencoded({ extended: true }));
 // routes
 app.get("/", (req, res) => {
   res.redirect("/blogs");
+});
+
+app.get("/sass", (req, res) => {
+  res.render("sass-component", { title: "Learn SASS" });
 });
 
 app.get("/about", (req, res) => {
